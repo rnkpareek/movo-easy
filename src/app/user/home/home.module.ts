d64398c560de25ef5347faa8bc611973c0324ea2
Ronak <ronak.scriptbees.com>
2019-02-25 21:06:38 +0530

    new changes

--- src/app/user/home/home.module.ts
+++ src/app/user/home/home.module.ts
@@ -12,6 +12,9 @@ import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { AlertModule } from 'ngx-bootstrap';
 import { HeaderComponent } from './header/header.component';
 import { TabsModule } from 'ngx-bootstrap';
+import { AboutComponent } from './about/about.component';
+import { PlanComponent } from './plan/plan.component';
+import { VendorsComponent } from './vendors/vendors.component';
 @NgModule({
   imports: [
     CommonModule,
@@ -22,6 +25,9 @@ import { TabsModule } from 'ngx-bootstrap';
   ],
   declarations: [HomeComponent,
     MainComponent,
-    HeaderComponent]
+    HeaderComponent,
+    AboutComponent,
+    PlanComponent,
+    VendorsComponent]
 })
 export class HomeModule { }
--- src/app/user/home/main-routing.ts
+++ src/app/user/home/main-routing.ts
@@ -1,9 +1,25 @@
 import { Routes } from '@angular/router';
 import {MainComponent} from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { PlanComponent } from './plan/plan.component';
+import { VendorsComponent } from './vendors/vendors.component';
+import {HomeComponent} from './home.component';
 
 export const mainRoute: Routes = [
     {
         path: '',
         component: MainComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: 'vendors',
+        component: PlanComponent
+      },
+      {
+        path: 'vendorsplan',
+        component: VendorsComponent
       }
 ];
